f9b96ed8bc0a4aeb980a30fd92a42d463b31e6f9
Shane Logsdon <shane@shanelogsdon.com>
2018-01-23 14:39:00 -0500

    update to latest parcel

--- src/fable-asset.js
+++ src/fable-asset.js
@@ -1,13 +1,37 @@
 const path = require('path');
+const {Asset} = require('parcel-bundler');
 // TODO: see if there is a way to clean up these requires
-const JSAsset = require('parcel-bundler/src/assets/JSAsset');
 const fs = require('parcel-bundler/src/utils/fs');
 const localRequire = require('parcel-bundler/src/utils/localRequire');
 
-class FableAsset extends JSAsset {
+class FableAsset extends Asset {
+  constructor(name, pkg, options) {
+    super(name, pkg, options);
+    this.type = 'js';
+  }
+
+  process() {
+    // We don't want to process this asset if the worker is in a warm up phase
+    // since the asset will also be processed by the main process, which
+    // may cause errors since rust writes to the filesystem.
+    if (this.options.isWarmUp) {
+      return;
+    }
+
+    return super.process();
+  }
+
+  async generate() {
+    // TODO: is reading the file here required?
+    const outputFile = this.name.replace(/\.(fsproj|fsx)$/, '.js');
+    const outputContent = await fs.readFile(outputFile);
+    return {
+      js: outputContent.toString(),
+    };
+  }
+
   async parse(code) {
-    await localRequire('babel-core', this.name);
-    const fable = await localRequire('fable-splitter', this.name);
+    const fableSplitter = await this.loadDeps();
 
     // TODO: read from config file and overwrite with the below
     const options = {
@@ -16,12 +40,19 @@ class FableAsset extends JSAsset {
     };
 
     // TODO: possible without temp file?
-    const output = await fable.default(options);
+    const output = await fableSplitter(options);
     const outputFile = this.name.replace(/\.(fsproj|fsx)$/, '.js');
     const outputContent = await fs.readFile(outputFile);
     this.contents = outputContent.toString();
 
     return await super.parse(this.contents);
+  }
+
+  async loadDeps() {
+
+    await localRequire('babel-core', this.name);
+    const fable = await localRequire('fable-splitter', this.name);
+    return fable.default;
   }
 }
 
