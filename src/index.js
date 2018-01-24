31c43ebd680b198e977c60b8eb9ed808f65df75e
Shane Logsdon <shane@shanelogsdon.com>
2018-01-24 18:52:27 -0500

    add fsx file support

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 module.exports = function(bundler) {
   bundler.addAssetType("fsproj", require.resolve("./fable-asset"));
   bundler.addAssetType("fs", require.resolve("./fable-asset"));
+  bundler.addAssetType("fsx", require.resolve("./fable-asset"));
 };
