9265f7873dd8d3bf0eb095cf04f3f923a0514714
Shane Logsdon <shane@shanelogsdon.com>
2018-01-23 23:55:24 -0500

    add basic error handling

--- src/fable-asset.js
+++ src/fable-asset.js
@@ -27,7 +27,7 @@ class FableAsset extends Asset {
   }
 
   async parse(code) {
-    const fableSplitter = await this.loadDeps();
+    const fableSplitter = await this.requireDependencies();
     let options = {
       entry: this.name,
       outDir: path.dirname(this.name),
@@ -39,7 +39,7 @@ class FableAsset extends Asset {
       })
     };
 
-    // read project config and use that as the base
+    // read project config, and use that as the base
     const config = await this.getConfig(["fable-splitter.config.js"]);
     if (config) {
       options = Object.assign(config, options);
@@ -47,21 +47,26 @@ class FableAsset extends Asset {
 
     const info = await fableSplitter(options);
 
+    // fable-splitter will bubble up errors, but it will also still
+    // display them. No need to display the errors via Parcel, so a
+    // simple message should do.
+    if (Array.isArray(info.logs.error) && info.logs.error.length > 0) {
+      throw new Error("Compilation error. See log above");
+    }
+
     // add compiled paths as dependencies for watch functionality
     // to trigger rebuild on F# file changes
     Array.from(info.compiledPaths).map(p =>
       this.addDependency(p, { includedInParent: true })
     );
 
-    // TODO: possible without temp file?
-    const outputFile = this.name.replace(/\.(fsproj|fsx)$/, ".js");
-    const outputContent = await fs.readFile(outputFile);
-    this.contents = outputContent.toString();
+    await this.populateContents();
 
     // `this.contents` becomes the new value for `this.ast`
     return this.contents;
   }
 
+  // final transformations before bundle is generated
   async transform() {
     if (this.options.minify) {
       await uglify(this);
@@ -74,7 +79,19 @@ class FableAsset extends Asset {
     };
   }
 
-  async loadDeps() {
+  generateErrorMessage(error) {
+    return (
+      path.relative(process.cwd(), this.options.rootDir) +
+      path.sep +
+      this.relativeName +
+      ": " +
+      error.message
+    );
+  }
+
+  // helpers
+
+  async requireDependencies() {
     // dotnet SDK tooling is required by Fable to operate successfully
     try {
       await commandExists("dotnet");
@@ -87,6 +104,17 @@ class FableAsset extends Asset {
     await localRequire("babel-core", this.name);
     const fable = await localRequire("fable-splitter", this.name);
     return fable.default;
+  }
+
+  async populateContents() {
+    // TODO: possible without temp file?
+    try {
+      const outputFile = this.name.replace(/\.(fsproj|fsx)$/, ".js");
+      const outputContent = await fs.readFile(outputFile);
+      this.contents = outputContent.toString();
+    } catch (e) {
+      throw new Error("Intermediate build file missing");
+    }
   }
 }
 
